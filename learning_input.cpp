4d37b6f28e623a037842c48f35d8623920bde70d
Anubhav-codes1 <adev63576@gmail.com>
2025-06-23 09:35:39 +0530

    Add files via upload
    Learnt how to take input from user in c++...

---
+++ learning_input.cpp
@@ -0,0 +1,13 @@
+// we'll learn ho to take input from the user and we'll build a program that will add two numbers:
+
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter your number 1:";
+    cin>>num1;
+    cout<<"enter your number 2:";
+    cin>>num2;
+    cout<<"the sum is:"<<num1+num2;
+    return 0;
+}
